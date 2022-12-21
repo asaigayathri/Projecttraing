c3edb1f09f88899b7c57bad5c98acec7338410ea
Dell <Dell@DESKTOP-FQC8UE8>
2022-12-21 07:06:07 +0530

    commit

---
+++ gittraing/src/gittraing/Trainggit.java
@@ -0,0 +1,11 @@
+package gittraing;
+
+public class Trainggit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("GitSystem ");
+ System.out.println("all S");
+	}
+
+}
